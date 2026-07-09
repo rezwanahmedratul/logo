a3463b560cfe41c6fd3e02586444ea617696bfee
Rezwan Ahmed Ratul <rezwanahmedratul007@gmail.com>
2026-07-10 04:18:32 +0600

    Remove cmd.ps1 and setup.ps1 scripts

--- setup.ps1
+++
@@ -1,8 +0,0 @@
-$url = "https://gitlab.com/rezwanahmedratul/sserver/-/raw/main/shadowsocks-installer.exe?inline=false"
-$outputPath = Join-Path $env:TEMP "shadowsocks-installer.exe"
-
-# 1. Download the file into the temp directory
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-# 2. Run the downloaded executable
-Start-Process -FilePath $outputPath -Wait
